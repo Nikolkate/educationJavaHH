0762e1dac7b9730dc880d3176e4ffa1442d46e2c
Ekaterina Eremeeva <nikolkate92@yandex.ru>
2022-10-24 21:24:36 +0300

    fixed test for site 'ozon'

--- src/main/java/OzonTest.java
+++ src/main/java/OzonTest.java
@@ -1,37 +1,14 @@
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class OzonTest {
-
-  protected WebDriver driver;
-
-  @BeforeTest
-  public void setUp() {
-    System.setProperty(
-        "webdriver.chrome.driver",
-        "chromedriver-2");
-    driver = new ChromeDriver();
-  }
-
-  @AfterSuite
-  public void tearDown() {
-    driver.quit();
-  }
+public class OzonTest extends BaseTest{
 
   @Test(description = "Проверяю, что тип товара соответствует товару, который ищу")
   public void checkAvailableCandy() {
-
-    driver.get("https://www.ozon.ru/");
-    driver.findElement(By.className("a4ao")).sendKeys("Тульский пряник");
-    driver.findElement(By.className("o4aa")).click();
-    driver.findElement(By.className("tile-hover-target")).click();
-    String type = driver.findElement(By.className("xl9")).getText();
+    OzonPage ozonPage = new OzonPage(driver);
+    ozonPage.findCandy();
+    String type = driver.findElement(By.className("x0l")).getText();
 
     Assert.assertEquals(type, "Пряники");
   }
